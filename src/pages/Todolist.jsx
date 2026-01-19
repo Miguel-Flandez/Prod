43f17a061e2950211a7b06928509bbf10d6bd5fe
Miguel Flandez <immfpnvs2@gmail.com>
2026-01-19 15:30:05 +0800

    refactored the array maps to allow for animation of list checking

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -66,25 +66,27 @@ export default function Todolist(){
             <Header/>
             <Warning prompt={prompt} setPrompt={setPrompt}/>
             <div className="flex justify-between w-full h-[80%]">
-                <div id="pending" className=" w-[45%] h-full m-auto bg-[#1211112f] rounded-xl relative overflow-auto" >
-                    <h1 className="text-center text-[2rem]">Pending</h1>
-                    <button className="hover:text-red-900 m-auto" onClick={()=>{localStorage.setItem('to-do-list',JSON.stringify([]));setodo([])}}>Reset</button>
-                    <button id="maximize" className="absolute top-[1rem] right-[1rem] p-[0.25rem] rounded-sm hover:bg-[#c4b7b72f] flex align-center">
-                        <i class="fa-solid fa-expand"></i>
-                    </button>
-                    {todo.map((item, index)=>{
-
-                        return !item.done && (
-                            <div key={"pending" + index} className={`${item.done && 'translate-x-full'} flex items-center gap-2 p-[1rem] transition-transform`}>
-                                <button className={`border-2 overflow-hidden rounded-[50%] w-[2rem] max-md:w-[1rem] h-[2rem] max-md:h-[1rem]`}
-                                onClick={()=>check(index)}>
-                                    <div className={`${item.done ? 'scale-200' : 'scale-0'} transition-all duration-500 rounded-[50%] bg-white w-full h-full `}></div>
-                                </button>
-                                <input className="text-[2rem] outline-0" placeholder="Title" value={item.title} onChange={(e)=>change(e,index)}></input>
-                            </div>  
-                        )
-                        
-                    })}
+                <div id="pending" className=" w-[45%] h-full m-auto bg-[#1211112f] rounded-xl relative z-10" >
+                    <div className="h-[90%] w-full overflow-auto overflow-x-hidden">
+                        <h1 className="text-center text-[2rem] max-sm:text-[1rem]">PENDING</h1>
+                        <button className="hover:text-red-900 m-auto" onClick={()=>{localStorage.setItem('to-do-list',JSON.stringify([]));setodo([])}}>Reset</button>
+                        <button id="maximize" className="absolute top-[1rem] max-md:top-[0.25rem] right-[1rem] max-md:right-[0.25rem] p-[0.25rem]
+                         rounded-sm hover:bg-[#c4b7b72f] flex align-center">
+                            <i class="fa-solid fa-expand max-sm:text-[0.75rem]"></i>
+                        </button>
+                        {todo.map((item, index)=>(
+                                <div key={"pending" + index} className={`${item.done && 'translate-x-full opacity-0 absolute'} flex items-center gap-2 p-[1rem] transition-all z-0`}>
+                                    <button className={`border-2 overflow-hidden rounded-[50%] min-w-[2rem] max-md:min-w-[1rem] h-[2rem] max-md:h-[1rem] group`}
+                                    onClick={()=>check(index)}>
+                                        <div className={`${item.done ? 'scale-200' : 'scale-0'} group-hover:scale-50 transition-all duration-500 rounded-[50%] bg-white w-full h-full `}></div>
+                                    </button>
+                                    <input className="text-[2rem] outline-0 max-md:text-[1rem]" placeholder="Title" value={item.title} onChange={(e)=>change(e,index)}></input>
+                                </div>  
+                            )
+                            
+                        )}
+                    </div>
+                    
                     
                     <button id="add" className="flex justify-between items-center p-[0.5rem] absolute bottom-1 left-1/2 -translate-x-1/2"
                     onClick={addTodo} >
@@ -93,19 +95,17 @@ export default function Todolist(){
                     
                 </div>
 
-                <div id="done" className=" w-[45%] h-full m-auto bg-[#1211112f] rounded-xl overflow-auto" >
-                        <h1 className="text-center text-[2rem]">Done</h1>
-                    {todo.map((item, index)=>{
-
-                        return Boolean(item.done) && (
-                            <div key={"done" + index} className="flex items-center gap-2 p-[1rem] bg-">
-                                <button className={`border-2 overflow-hidden rounded-[50%] w-[2rem] max-md:w-[1rem] h-[2rem] max-md:h-[1rem]`}
+                <div id="done" className=" w-[45%] h-full m-auto bg-[#1211112f] rounded-xl overflow-auto overflow-x-hidden z-10" >
+                        <h1 className="text-center text-[2rem] max-sm:text-[1rem]">Done</h1>
+                    {todo.map((item, index)=>(
+                            <div key={"done" + index} className={`${!item.done && 'translate-x-full opacity-0 absolute'} flex items-center gap-2 p-[1rem] transition-all z-0`}>
+                                <button className={`border-2 overflow-hidden rounded-[50%] min-w-[2rem] max-md:min-w-[1rem] h-[2rem] max-md:h-[1rem]`}
                                 onClick={()=>check(index)}>
                                     <div className={`transition-all duration-500 rounded-[50%] bg-white w-full h-full scale-200`}></div>
                                 </button>
-                                <span className="text-[2rem] line-through">{item.title}</span>
+                                <span className="text-[2rem] line-through max-md:text-[1rem]">{item.title}</span>
                             </div>  
-                    )})}
+                    ))}
                     
                 </div>
 
